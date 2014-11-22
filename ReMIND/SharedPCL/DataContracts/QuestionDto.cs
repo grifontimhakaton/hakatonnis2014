fb959de8ec2efa4e87c9c706cc339d68b0db267f
grifontimhakaton <grifontimhakaton@mailinator.com>
2014-11-22 18:17:51 +0100

    added shared contracts

---
+++ ReMIND/SharedPCL/DataContracts/QuestionDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SharedPCL.Enums;
+
+namespace SharedPCL.DataContracts
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; } // Remove if not needed
+        public string Text { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public List<string> Answers { get; set; }
+
+    }
+}
---
+++ ReMIND/SharedPCL/Enums/CategoryTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedPCL.Enums
+{
+    public enum CategoryTypes
+    {
+        Mathematics = 1,
+        Geography,
+        Medicine,
+        Art
+    }
+}
