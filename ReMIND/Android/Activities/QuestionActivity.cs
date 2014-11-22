87cf3152d93739bcedab1324cfaa945c4655d209
grifontimhakaton <grifontimhakaton@mailinator.com>
2014-11-22 19:27:12 +0100

    Android added permissions and 2 activities

---
+++ ReMIND/Android/Activities/QuestionActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Android.Activities
+{
+    [Activity]
+    public class QuestionActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Question);
+        }
+    }
+}
+
