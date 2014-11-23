ed1e7352771bed7bf379a34912f77436d1c9d370
grifontimhakaton <grifontimhakaton@mailinator.com>
2014-11-23 07:45:14 +0100

    resize prototype

--- ReMIND/Android/Activities/QuestionActivity.cs
+++ ReMIND/Android/Activities/QuestionActivity.cs
@@ -52,7 +52,8 @@ namespace ReMinder.Activities
                 if(questionList.Count > 0)
                 {
                     var question = questionList[0];
-                    txtQuestion.Text = question.QuestionText;
+                    txtQuestion.Text = question.QuestionText + "TEst test test test test";
+                    RefitText(txtQuestion.Text, 700);
                     if (question.QuestionAnswers.Count > 1)
                     {
                         listAnswers.Adapter = new AnswerAdapter(this, question.QuestionAnswers.Select(x => x.QuestionAnswerText).ToArray());
@@ -64,6 +65,34 @@ namespace ReMinder.Activities
                     }
                 }
             }
+        }
+
+        private void RefitText(String text, int textWidth)
+        {
+            if (textWidth <= 0)
+                return;
+            var mTestPaint = new Android.Graphics.Paint();
+            mTestPaint.Set(txtQuestion.Paint);
+
+            int targetWidth = textWidth - txtQuestion.PaddingLeft - txtQuestion.PaddingRight;
+            float hi = 100;
+            float lo = 2;
+            float threshold = 0.5f; // How close we have to be
+
+            mTestPaint.Set(txtQuestion.Paint);
+
+            while ((hi - lo) > threshold)
+            {
+                float size = (hi + lo)/2;
+                mTestPaint.TextSize = size;
+                if (mTestPaint.MeasureText(text) >= targetWidth)
+                    hi = size; // too big
+                else
+                    lo = size; // too small
+            }
+            // Use lo so that we undershoot rather than overshoot
+            //txtQuestion.TextSize = (Android.Util.TypedValue.ComplexToDimensionPixelSize(), lo);
+            txtQuestion.TextSize =  (int)lo;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
